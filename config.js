ae0fd92349c5eb2baf0a4a9e1720ee8b9d5441b1
georgis3 <65974205+georgis3@users.noreply.github.com>
2026-02-02 00:18:25 -0600

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dm9nxb98q/raw/upload/v1770012592/Jnq9wPDoDKg_za37ob", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dm9nxb98q/video/upload/v1770013066/angga-renggana-t_sixpence-none-the-richer-kiss-me_gnm8lz.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
